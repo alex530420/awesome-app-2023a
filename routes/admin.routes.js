f490b74318287f2c5c7ad3bea05620a68f6ee91b
ALEX <alejandrocarreon530420@gmail.com>
2023-06-01 02:13:29 +0000

    🧩 Vistas Parciales

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -16,15 +16,14 @@ export const products = [];
 
 // GET /add-product
 router.get('/add-product', (req, res, next) => {
-  // Si la petición es post pasamos el siguiente
-  // Middleware
-  if(req.method === "POST") return next();
-
-  // Servimos el formulario
+// Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.render('add-product');
-  //console.log(`ROOT_DIR: ${ROOT_DIR}`);
-  //res.sendFile(path.join(ROOT_DIR,'views','add-product.hbs'));
+  res.render('add-product', {
+    addProduct: `active`, 
+    viewStyle: '/css/product.css',
+    docTitle:"Add Product"});
+  
+  
 });
 // POST /add-product
 router.post('/add-product', (req, res) => {
