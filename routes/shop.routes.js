0f3728e45510d54a7ae138e08b33089704128cf0
ALEX <alejandrocarreon530420@gmail.com>
2023-06-02 01:48:01 +0000

    Renderizado condicional❓

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -17,7 +17,13 @@ router.get('/', (req, res)=>{
   // Mostrando productos en memoria
   console.log(products);
   console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop', {shop: 'active', docTitle:"Shop"});
+  res.render('shop', { 
+    shop: 'active', 
+    docTitle:"Shop",
+    viewStyle: '/css/product.css',
+    isProductsListEmpty: products.length === 0,
+    products
+  });
 });
 
 // GET /about
